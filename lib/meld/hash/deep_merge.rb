bf6da3bc0acb547119224ae2758907f9aeee8596
Les Aker <me@lesaker.org>
2014-12-25 23:02:31 -0500

    add harsh merge

--- lib/meld/hash/deep_merge.rb
+++ lib/meld/hash/deep_merge.rb
@@ -2,11 +2,15 @@ module Kernel
   # From https://gist.github.com/ku/e17e3d65ec4c37b60149
   # From http://stackoverflow.com/questions/9381553/ruby-merge-nested-hash
   class Hash
-    def deep_merge(second)
+    def deep_merge!(second)
       merger = proc do |_, v1, v2|
         [v1, v2].all? { |x| x.is_a? Hash } ? v1.merge(v2, &merger) : v2
       end
-      merge(second, &merger)
+      merge!(second, &merger)
+    end
+
+    def deep_merge(second)
+      dup.deep_merge! second
     end
   end
 end
